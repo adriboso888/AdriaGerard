1826a05781b413ef92496862990b6557c1e6300c
adriboso888 <adriaboix@inspladelestany.cat>
2022-12-03 12:47:45 +0100

    project

--- projecte/footer.js
+++ projecte/footer.js
@@ -3,14 +3,14 @@ footer.innerHTML = `
 <footer class="text-center footer-style">
         <div class="container">
             <div class="row">
-                <div class="col-md-4 footer-col">
+                <div class="col col-md-4 footer-col">
                     <h3>Creadors</h3>
                     <p>
                         Adrià Boix<br />
                         Gerard Trabazos
                     </p>
                 </div>
-                <div class="col-md-4">
+                <div class="col col-md-4">
                     <h3>Creació pàgina web</h3>
                     <ul class="list-inline">
                         <li>
@@ -18,7 +18,7 @@ footer.innerHTML = `
                         </li>
                     </ul>
                 </div>
-                <div class="col-md-4 footer-col ">
+                <div class="col col-md-4 footer-col ">
                     <h3>Descripció</h3>
                     <div class="container-fluid">
                       <ul class="navbar-nav">
